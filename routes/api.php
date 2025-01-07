386548335c1a054fc6f81ebe2491bc133475cc4e
David Ortega <davidortegacadena@gmail.com>
2025-01-07 16:36:20 -0500

    :art: Changed auth:api to const

--- routes/api.php
+++ routes/api.php
@@ -3,20 +3,21 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 
+const MIDDLEWARE_CONST = "auth:api";
 
 Route::group([
 
     'middleware' => 'api',
     'prefix' => 'auth'
 
-], function ($router) {
+], function () {
 
     Route::post('register', [AuthController::class,'register']);
     Route::post('login', [AuthController::class,'login']);
 
-    Route::post('logout', [AuthController::class,'logout'])->middleware('auth:api');
-    Route::post('refresh', [AuthController::class,'refresh'])->middleware('auth:api');
-    Route::post('profile', [AuthController::class,'profile'])->middleware('auth:api');
+    Route::post('logout', [AuthController::class,'logout'])->middleware(MIDDLEWARE_CONST);
+    Route::post('refresh', [AuthController::class,'refresh'])->middleware(MIDDLEWARE_CONST);
+    Route::post('profile', [AuthController::class,'profile'])->middleware(MIDDLEWARE_CONST);
 
 });
 
