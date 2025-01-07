7183481f594893ace86789faded67207de8c8c59
David Ortega <davidortegacadena@gmail.com>
2025-01-07 10:49:16 -0500

    :sparkles: initial commit with migrations

---
+++ database/migrations/2024_12_26_174522_offer.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('offer', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('brand_id')->references('id')->on('brand');
+            $table->string('title');
+            $table->string('description');
+            $table->string('image')->nullable();
+            $table->double('price');
+            $table->double('discount');
+            $table->string('discount_type');
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('offer');
+    }
+};
---
+++ database/migrations/2024_12_26_174527_invoice.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoice', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('brand_id')->references('id')->on('brand');
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->foreignId('offer_id')->references('id')->on('offer');
+            $table->date('date');
+            $table->double('total');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoice');
+    }
+};
